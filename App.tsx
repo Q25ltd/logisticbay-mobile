7268bcf6430f1b3daf777d4d093881f6150ade50
Q25ltd <q25limited@gmail.com>
2026-04-28 18:42:51 +0100

    fix: proper hook order in AppNavigator, InitialScreen for shift restore

--- App.tsx
+++ App.tsx
@@ -2,33 +2,30 @@ import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ActivityIndicator, View } from "react-native";
-
 import { AuthProvider, useAuth } from "./src/AuthContext";
 import { ShiftProvider, useShift } from "./src/ShiftContext";
-
 import LoginScreen          from "./src/screens/LoginScreen";
 import HomeScreen           from "./src/screens/HomeScreen";
 import HistoryScreen        from "./src/screens/HistoryScreen";
 import ShiftDetailScreen    from "./src/screens/ShiftDetailScreen";
-import JobsScreen         from "./src/screens/JobsScreen";
-import JobDetailScreen    from "./src/screens/JobDetailScreen";
+import JobsScreen           from "./src/screens/JobsScreen";
+import JobDetailScreen      from "./src/screens/JobDetailScreen";
 import ChangePinScreen      from "./src/screens/ChangePinScreen";
-import ChecklistScreen      from "./src/screens/ChecklistScreen";
-import DeliveriesScreen     from "./src/screens/DeliveriesScreen";
-import EndSegmentScreen     from "./src/screens/EndSegmentScreen";
-
-import NewStartShiftScreen from "./src/screens/StartShiftScreen";
-import HolidayScreen from "./src/screens/HolidayScreen";
+import NewStartShiftScreen  from "./src/screens/StartShiftScreen";
+import HolidayScreen        from "./src/screens/HolidayScreen";
+import { COLOURS } from "./src/components";
 import {
   StartShiftScreen as OldStartShiftScreen,
   EndShiftScreen,
   ReviewScreen,
+  EndSegmentScreen,
 } from "./src/screens/ShiftScreens";
-
-import { COLOURS } from "./src/components";
+import { ChecklistScreen }  from "./src/screens/ChecklistScreen";
+import { DeliveriesScreen } from "./src/screens/DeliveriesScreen";
 
 const Stack = createNativeStackNavigator();
 
+// Shows spinner then redirects based on shift state
 function InitialScreen({ navigation }: { navigation: any }) {
   const { draft, draftRestored } = useShift() as any;
   useEffect(() => {
@@ -57,31 +54,26 @@ function AppNavigator() {
     );
   }
 
-  const { draft, draftRestored } = useShift() as any;
-  const hasActiveShift = draftRestored && !!draft?.shiftId;
-
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {user ? (
           <>
-            <Stack.Screen name="Initial"         component={InitialScreen} />
-            <Stack.Screen name="Home"           component={HomeScreen} />
-            <Stack.Screen name="StartShift"     component={NewStartShiftScreen} />
-            <Stack.Screen name="TruckChecklist" component={ChecklistScreen}
-              initialParams={{ type: "truck" }} />
-            <Stack.Screen name="TrailerChecklist" component={ChecklistScreen}
-              initialParams={{ type: "trailer" }} />
-            <Stack.Screen name="Deliveries"     component={DeliveriesScreen} />
-            <Stack.Screen name="EndSegment"     component={EndSegmentScreen} />
-            <Stack.Screen name="EndShift"       component={EndShiftScreen} />
-            <Stack.Screen name="Review"         component={ReviewScreen} />
-            <Stack.Screen name="History"        component={HistoryScreen} />
-            <Stack.Screen name="ChangePin"       component={ChangePinScreen} />
-            <Stack.Screen name="ShiftDetail"     component={ShiftDetailScreen} />
-            <Stack.Screen name="Jobs"            component={JobsScreen} />
-            <Stack.Screen name="JobDetail"       component={JobDetailScreen} />
-            <Stack.Screen name="Holidays"        component={HolidayScreen} />
+            <Stack.Screen name="Initial"          component={InitialScreen} />
+            <Stack.Screen name="Home"             component={HomeScreen} />
+            <Stack.Screen name="StartShift"       component={NewStartShiftScreen} />
+            <Stack.Screen name="TruckChecklist"   component={ChecklistScreen} initialParams={{ type: "truck" }} />
+            <Stack.Screen name="TrailerChecklist" component={ChecklistScreen} initialParams={{ type: "trailer" }} />
+            <Stack.Screen name="Deliveries"       component={DeliveriesScreen} />
+            <Stack.Screen name="EndSegment"       component={EndSegmentScreen} />
+            <Stack.Screen name="EndShift"         component={EndShiftScreen} />
+            <Stack.Screen name="Review"           component={ReviewScreen} />
+            <Stack.Screen name="History"          component={HistoryScreen} />
+            <Stack.Screen name="ChangePin"        component={ChangePinScreen} />
+            <Stack.Screen name="ShiftDetail"      component={ShiftDetailScreen} />
+            <Stack.Screen name="Jobs"             component={JobsScreen} />
+            <Stack.Screen name="JobDetail"        component={JobDetailScreen} />
+            <Stack.Screen name="Holidays"         component={HolidayScreen} />
           </>
         ) : (
           <Stack.Screen name="Login" component={LoginScreen} />
